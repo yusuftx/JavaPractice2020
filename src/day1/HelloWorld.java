5e9155fe051ce03268ba67b6b85ac3c4b1acf7f5
yusuftx <yusuftx@gmail.com>
2020-01-06 01:28:32 -0600

    second commit

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 		System.out.print("I like Java");
 		System.out.println(" This is object oriented programing language");
 		
